5ada19ed8820b8fe2a27c469500681e9f0472b0a
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-02 22:28:42 +0100

    ui design

---
+++ app/src/main/java/com/lemzeeyyy/movieapp/utils/Credentials.java
@@ -0,0 +1,7 @@
+package com.lemzeeyyy.movieapp.utils;
+
+public class Credentials {
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String API_KEY = "528aa63374c88d874c52737b28bfafb9";
+    public static boolean POPULAR = false;
+}
